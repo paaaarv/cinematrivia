2db241dfecd36dd8594bd4659bf8569c0f12a1e2
paaaarv <pkumar@trinity.edu>
2021-06-26 16:53:09 -0500

    response to json in order to map through and save to db

--- cinematrivia-api/app/controllers/trivia_controller.rb
+++ cinematrivia-api/app/controllers/trivia_controller.rb
@@ -4,8 +4,17 @@ class TriviaController < ApplicationController
     def index
         response = RestClient::Request.execute(method: :get, url: 'https://imdb8.p.rapidapi.com/title/get-trivia?tconst=tt0944947', headers:{
             "x-rapidapi-key": "0ac3f5fb74msh43d4672e7cf7114p126195jsn8a05f0b35269",
-		"x-rapidapi-host": "imdb8.p.rapidapi.com"
+		"x-rapidapi-host": "imdb8.p.rapidapi.com",
+        "accept": "application/json",
+        "Content-Type": "application/json"
             })
-        render json: response
+            binding.pry
+        data = JSON.parse response
+        data["spoilt"].map do |trivia|
+            binding.pry
+        end
+
+        binding.pry
+        render json: data
     end
 end
