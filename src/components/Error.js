bb1bfddfd80dbc6779a959a7453ae8631bddbda8
paaaarv <pkumar@trinity.edu>
2022-01-23 20:00:16 -0600

    changes to error component still needed to make alert

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+// need to make a alert popup instead for error messages, stay on the same component
+
 export default class Error extends React.Component{
     render(){
         return(
-            <div> 
+            <div>
             <h3>sorry! we couldn't find your movie, please hit search and try again!</h3>
             </div>
         )
